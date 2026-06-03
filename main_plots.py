ab6ba9d922a2a218d0e56d39756d2618d4e58e83
zayanvali <zayan.vali@icloud.com>
2026-06-03 18:59:32 +0100

    Add files via upload

--- main_plots.py
+++ main_plots.py
@@ -23,4 +23,34 @@ plt.legend()
 plt.show()
 
 #Plot multiple graphs on a single plot
-plt.plot(x, y, linewidth = 5, label = "line 1", color = "green")
+x1 = [3, 5, 6, 14, 18]
+y1 = [7, 8, 10, 16, 21]
+plt.plot(x, y, linewidth = 5, label  = "line 1", color = "purple")
+plt.plot(x1, y1, linewidth = 5, label = "line 2", color = "red")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.title("Multiple Lines Plot")
+plt.legend()
+plt.show()
+
+#Polynomial functions plot
+x = np.arange(0, 10, 0.2)
+print(x)
+y1 = x**2
+y2 = x**3
+plt.plot(x, y1, linewidth = 7, label = "line 1", color = "blue")
+plt.plot(x, y2, linewidth = 7, label = "line 2", color = "pink")
+plt.title("Polynomial Functions")
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.legend()
+plt.show()
+
+#Plot bar graph
+subjects = ["Maths", "Physics", "Spanish", "Geography", "Computer"]
+time = [120, 90, 90, 60, 90]
+plt.bar(subjects, time, color = "green")
+plt.xlabel("Subject")
+plt.ylabel("Time")
+plt.title("Bar Graph")
+plt.show()
